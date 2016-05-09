8ac31eb28f3ad7724f3b6abfee43f4eec5cd2068
Seung Yeon Joo <wntmddussla@hotmail.com>
2016-05-09 15:57:14 -0700

    Update ActiveState.java

--- src/ActiveState.java
+++ src/ActiveState.java
@@ -1,9 +1,16 @@
 
 public class ActiveState implements RideState {
-	
+	private Ride ride;
+	public ActiveState(Ride r) {
+		this.ride = r;
+	}
 	@Override
 	public void getInfo(){
 		
+	}
+	
+	public void progressState() {
+		ride.setRideState(new CompleteState(ride));
 	}
 
 }
