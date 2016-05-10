9aa2671a1811e35955fc8991419df3d6fc6bcc16
Seung Yeon Joo <wntmddussla@hotmail.com>
2016-05-09 23:57:00 -0700

    Update CompleteState.java

--- src/CompleteState.java
+++ src/CompleteState.java
@@ -1,11 +1,19 @@
+import java.text.SimpleDateFormat;
 
 public class CompleteState implements RideState {
-	
-	@Override
-	public void getInfo(){
-		
+	private Ride ride;
+	public CompleteState(Ride r) {
+		this.ride = r;
+	}
+	public void getInfo() {
+		SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd-hh-mm");
+		String formatted = format1.format(ride.getDate());
+		System.out.println(ride.getStartTime() + " "+ formatted + " " + ride.getStartLocation() + " " + ride.getDriver() + " " + ride.getRideState());
 	}
 	public void progressState() {
-		
+		System.out.println("End of progressState!");
 	}
+
+
+
 }
