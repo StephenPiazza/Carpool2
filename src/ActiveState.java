6fb3034b033033a9f3c9bd963c539ec2958528ea
Seung Yeon Joo <wntmddussla@hotmail.com>
2016-05-09 23:55:56 -0700

    Update ActiveState.java

--- src/ActiveState.java
+++ src/ActiveState.java
@@ -1,16 +1,17 @@
+import java.text.SimpleDateFormat;
 
 public class ActiveState implements RideState {
-	protected Ride ride;
+	private Ride ride;
 	public ActiveState(Ride r) {
 		this.ride = r;
-	}
-	@Override
-	public void getInfo(){
-		
 	}
 	
 	public void progressState() {
 		ride.setRideState(new CompleteState(ride));
 	}
-
+	public void getInfo() {
+		SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd-hh-mm");
+		String formatted = format1.format(ride.getDate());
+		System.out.println(ride.getStartTime() + " "+ formatted + " " + ride.getStartLocation() + " " + ride.getDriver() + " " + ride.getRideState());
+	}
 }
