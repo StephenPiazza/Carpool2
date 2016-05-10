cab622a87d36b548819c54510d1b47177c392af4
Seung Yeon Joo <wntmddussla@hotmail.com>
2016-05-09 23:56:23 -0700

    Update WaitingState.java

--- src/WaitingState.java
+++ src/WaitingState.java
@@ -1,17 +1,17 @@
+import java.text.SimpleDateFormat;
 
 public class WaitingState implements RideState {
-	protected Ride ride;
-	
-	public WaitingState(Ride ride){
-		this.ride = ride;
+	private Ride ride;
+	public WaitingState(Ride r) {
+		this.ride = r;
 	}
 	
+	public void getInfo() {
+		SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd-hh-mm");
+		String formatted = format1.format(ride.getDate());
+		System.out.println(ride.getStartTime() + " "+ formatted + " " + ride.getStartLocation() + " " + ride.getDriver() + " " + ride.getRideState());
+	}
 	public void progressState() {
 		ride.setRideState(new ActiveState(ride));
 	}
-	@Override
-	public void getInfo(){
-		
-	}
-
 }
